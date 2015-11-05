73b1d5b64eefbf79a2282aa85a49fb5839f0a471
hd132610 <ray234@naver.com>
2015-11-05 11:37:10 +0900

    레이스

---
+++ JavaPractice/src/org/dimigo/thread/Runner.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * 		|_ Runner
+ *  
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 5.
+ * </pre>
+ *
+ * @author H.J.Park
+ * @version 1.0.0
+ * 
+ */
+public class Runner extends Thread {
+	private String name;
+
+	public Runner() {
+		super();
+	}
+	
+	public Runner(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public void run() {
+		System.out.println(name + " 출발");
+		for(int i=10; i>=0; i--) {
+			System.out.println(name + " " + i*10 + "미터");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println(name + " 골인");
+	}
+	
+}
